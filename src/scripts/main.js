39bbe11109833cc6bcc28955ffc69cc5f97f6808
fvallrxi <fvallrxii@gmail.com>
2026-09-23 13:50:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const burger = document.querySelector('.header__hero__menu');
+const closeButton = document.querySelector('.menu__close');
+const menuLinks = document.querySelectorAll('.menu__link, .menu__btn');
+
+function toggleMenu(isOpen) {
+  menu.classList.toggle('menu--open', isOpen);
+  document.body.classList.toggle('no-scroll', isOpen);
+}
+
+burger.addEventListener('click', () => toggleMenu(true));
+closeButton.addEventListener('click', () => toggleMenu(false));
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => toggleMenu(false));
+});
